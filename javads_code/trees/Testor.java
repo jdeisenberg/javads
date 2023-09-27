b2b4a1907a5a3f40eb8a6617ef31a4e55a9cd54e
J. David Eisenberg <jdavid.eisenberg@gmail.com>
2023-09-26 18:21:01 -0700

    Commit to allow changing branches safely.

--- javads_code/trees/Testor.java
+++ javads_code/trees/Testor.java
@@ -1,36 +1,13 @@
 import java.util.Iterator;
 
-public class Testor implements Iterator<Integer>, Iterable<Integer> {
-
-    Integer value;
-    
-    public Testor() {
-       // value = 1;
-    }
-    
-    public Iterator<Integer> iterator() {
-        this.value = 1;
-        return this;
-    }
- 
-    public boolean hasNext() {
-        return (value < 128);
-    }
-    
-    public Integer next() {
-        value *= 2;
-        return value;
-    }
+public class Testor {
 
     public static void main(String[] args) {
-        Testor t = new Testor();
-        for (Integer i: t) {
-            System.out.println(i);
-        }
-        System.out.println("-------------");
-        for (Integer i: t) {
-            System.out.println(i);
-        }
+        AVLTree<String, String> tree = new AVLTree<>();
+        tree.put("Japan", "Tokyo");
+        tree.put("France", "Paris");
+        
+        AVLTree.AVLTreeNode node = (AVLTree.AVLTreeNode) tree.get("Japan", tree.root);
        
     }
 }
